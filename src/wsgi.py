5f8500fb4674866ef7dbb7d64214cae8f22cf1a1
Mateusz <matimilan@o2.pl>
2013-07-23 15:07:18 +0200

    sobota nowy commit

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,13 +1,32 @@
 import os, sys
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+# path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+#
+# if path not in sys.path:
+#    sys.path.append(path)
+#
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+#
+#
+# import django.core.handlers.wsgi
+#
+# application = django.core.handlers.wsgi.WSGIHandler()
 
-if path not in sys.path:
-   sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+PROJECT_ROOT = os.path.join(PROJECT_PATH, 'src')
+LIB_ROOT = os.path.join(PROJECT_ROOT, 'libs')
+if PROJECT_PATH not in sys.path:
+    sys.path.insert(0, PROJECT_PATH)
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+if LIB_ROOT not in sys.path:
+    sys.path.insert(0, LIB_ROOT)
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
-import django.core.handlers.wsgi
-
-application = django.core.handlers.wsgi.WSGIHandler()
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
